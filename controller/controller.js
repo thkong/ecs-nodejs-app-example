e125df67452d4c1f51194c62d07aca5a806e9120
thkong <thkong@smobile.kr>
2020-02-07 13:37:57 +0900

    test 100

--- controller/controller.js
+++ controller/controller.js
@@ -2,7 +2,13 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", (req, res) => {
-  res.status(200).json({ success: true, data: "healthy!!!!!b" });
+  var result = 0;
+  for (var i = 0; i < 100; i++) {
+    result += i;
+  }
+  res
+    .status(200)
+    .json({ success: true, data: "healthy!!!!!b" + result.toString() });
 });
 
 module.exports = router;
